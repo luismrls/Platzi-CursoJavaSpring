ec8d458aa652bda37e08fd95f7518ff59d8d6b82
Luis Morales <dev.luismorales@gmail.com>
2022-07-11 16:33:09 -0500

    first commit

---
+++ src/main/java/llmoraleslearn/curseplatzi/marker/MarkerApplication.java
@@ -0,0 +1,13 @@
+package llmoraleslearn.curseplatzi.marker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkerApplication.class, args);
+	}
+
+}
