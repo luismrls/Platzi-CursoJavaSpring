9b9c4120c3617beef6c39c9049c09ddb71a66d88
Luis Morales <dev.luismorales@gmail.com>
2022-07-14 11:45:31 -0500

    Se crearon los mappers

---
+++ src/main/java/llmoraleslearn/curseplatzi/marker/persistence/mappers/MapperCategoria.java
@@ -0,0 +1,24 @@
+package llmoraleslearn.curseplatzi.marker.persistence.mappers;
+
+import llmoraleslearn.curseplatzi.marker.domain.Categoria;
+import llmoraleslearn.curseplatzi.marker.persistence.entities.Category;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface MapperCategoria {
+
+    @Mappings({
+            @Mapping(source = "idCategory", target = "categoriasId"),
+            @Mapping(source = "description", target = "categoria"),
+            @Mapping(source = "state", target = "activo")
+    })
+    Categoria toCategoria(Category category);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productList", ignore = true)
+    Category toCategory(Categoria categoria);
+
+}
---
+++ src/main/java/llmoraleslearn/curseplatzi/marker/persistence/mappers/MapperProducto.java
@@ -0,0 +1,30 @@
+package llmoraleslearn.curseplatzi.marker.persistence.mappers;
+
+import llmoraleslearn.curseplatzi.marker.domain.Producto;
+import llmoraleslearn.curseplatzi.marker.persistence.entities.Product;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {MapperCategoria.class})
+public interface MapperProducto {
+
+    @Mappings({
+            @Mapping(source = "idProduct", target = "productoId"),
+            @Mapping(source = "name", target = "nombre"),
+            @Mapping(source = "IdCategory", target = "categoriaId"),
+            @Mapping(source = "salePrice", target = "precio"),
+            @Mapping(source = "quantityStock", target = "stock"),
+            @Mapping(source = "state", target = "activo"),
+            @Mapping(source = "category", target = "categoria"),
+    })
+    Producto toProducto(Product product);
+    List<Producto> toProductos(List<Product> products);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "barCode", ignore = true)
+    Product toProduct(Producto producto);
+}
