c3b3b2ef016b4c486b6fc027e12648a193eb625d
Luis Morales <dev.luismorales@gmail.com>
2022-07-14 10:30:10 -0500

    Se hicieron algunos query methods

--- src/main/java/llmoraleslearn/curseplatzi/marker/persistence/ProductRepository.java
+++ src/main/java/llmoraleslearn/curseplatzi/marker/persistence/ProductRepository.java
@@ -4,6 +4,7 @@ import llmoraleslearn.curseplatzi.marker.persistence.crud.ProductCrudRepository;
 import llmoraleslearn.curseplatzi.marker.persistence.entities.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
 
@@ -11,5 +12,13 @@ public class ProductRepository {
 
     public List<Product> getAll() {
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> findByCategory(int idCategory) {
+        return  productCrudRepository.findByIdCategory(idCategory);
+    }
+
+    public Optional<List<Product>> getLessThat(int quantity) {
+        return  productCrudRepository.findByQuantityStockLessThanAndState(quantity, true);
     }
 }
--- src/main/java/llmoraleslearn/curseplatzi/marker/persistence/crud/ProductCrudRepository.java
+++ src/main/java/llmoraleslearn/curseplatzi/marker/persistence/crud/ProductCrudRepository.java
@@ -3,5 +3,13 @@ package llmoraleslearn.curseplatzi.marker.persistence.crud;
 import llmoraleslearn.curseplatzi.marker.persistence.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository extends CrudRepository<Product, Integer> {
+
+    List<Product> findByIdCategory(int idCategoria);
+
+    Optional<List<Product>> findByQuantityStockLessThanAndState(int quantityStock, boolean state);
+
 }
