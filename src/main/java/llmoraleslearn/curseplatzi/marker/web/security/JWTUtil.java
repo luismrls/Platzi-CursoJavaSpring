880fe6384eefe742ff2ab01504581c33b585985f
Luis Morales <dev.luismorales@gmail.com>
2022-07-15 16:30:07 -0500

    se añadió la autorizacion a los servicios con jwt

--- src/main/java/llmoraleslearn/curseplatzi/marker/web/security/JWTUtil.java
+++ src/main/java/llmoraleslearn/curseplatzi/marker/web/security/JWTUtil.java
@@ -1,5 +1,6 @@
 package llmoraleslearn.curseplatzi.marker.web.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,4 +21,21 @@ public class JWTUtil {
                 .signWith(SignatureAlgorithm.HS256, KEY)
                 .compact();
     }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        return userDetails.getUsername().equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
+    }
+
 }
